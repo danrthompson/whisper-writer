25c8b80c5d605da9deffcc11d652b42bf1cef904
Dan Thompson <danielrthompsonjr@gmail.com>
2023-06-16 01:01:05 -0400

    fixed small issue

--- src/start_background_process.zsh
+++ src/start_background_process.zsh
@@ -1,4 +1,4 @@
-source ~/Code/Tools/whisper-writer/venv/bin/activate
+source ~/Code/Tools/whisper-writer/.venv/bin/activate
 nohup python -u ~/Code/Tools/whisper-writer/src/km_handler.py >> ~/Code/Tools/whisper-writer/nohup.out 2>&1 &
 cd ~/Code/Tools/whisper-writer
 tail -f nohup.out
