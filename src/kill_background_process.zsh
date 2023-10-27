2e71eff7634ced82fb87a1086ea696dfeb85dad5
Dan Thompson <danielrthompsonjr@gmail.com>
2023-10-27 15:59:11 -0400

    commit

--- src/kill_background_process.zsh
+++ src/kill_background_process.zsh
@@ -1,2 +1,24 @@
+killed_process=false
 pid=$(ps aux | grep "src/km_handler" | grep -v grep | awk '{print $2}')
-if [ ! -z "$pid" ]; then kill $pid; echo "Process killed"; else echo "Process not found"; fi
+if [ ! -z "$pid" ]; then
+    kill $pid
+    killed_process=true
+else
+    killed_process=false
+fi
+if [ "$killed_process" = true ]; then
+    echo "Killed process"
+else
+    echo "No process to kill"
+fi
+
+while [ "$killed_process" = true ]; do
+    sleep 1
+    pid=$(ps aux | grep "src/km_handler" | grep -v grep | awk '{print $2}')
+    if [ ! -z "$pid" ]; then
+        kill $pid
+        killed_process=true
+    else
+        killed_process=false
+    fi
+done
--- src/start_background_process.zsh
+++ src/start_background_process.zsh
@@ -1,4 +1,3 @@
+~/Code/Tools/cloned/whisper-writer/src/kill_background_process.zsh
 source ~/Code/Tools/cloned/whisper-writer/.venv/bin/activate
 nohup python -u ~/Code/Tools/cloned/whisper-writer/src/km_handler.py >> ~/Code/Tools/cloned/whisper-writer/nohup.out 2>&1 &
-cd ~/Code/Tools/cloned/whisper-writer
-tail -f nohup.out
